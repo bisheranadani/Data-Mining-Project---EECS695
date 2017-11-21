98118c5cda44d53ca050f6c1e7613e88f02d422b
Bisher Anadani <bisher.anadani@ku.edu>
2017-11-20 20:54:20 -0600

    implemented successful import of data to pandas

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
-def read_word(fileobj):
-    for line in fileobj:
-        for token in line.split():
-            yield token
+import numpy as np
+import pandas as pd
+from itertools import islice
+
+
 def main():
-    print "Hello, World!"
-    filename = raw_input("Please input the name of the file: ")
-    print filename+' is the name of the file'
+    print('Hello World\n')
+    data = pd.read_csv('test.txt', delim_whitespace=True, header=None, skiprows=2)
+    print (data)
 
-    with open(filename, 'r') as file:
-        words = read_word(file)
-        for i in range(50):
-            aword = next(words)
-            print aword
+    column_names = ''
+    with open('test.txt') as file:
+        for line in islice(file, 1, 2):
+            print(line)
+            column_names = line
 
+    print(column_names.split())
+    column_names = column_names.split()[1:-1]
+    print(column_names)
+    data.columns = column_names
+    print(data)
+# def read_word(fileobj):
+#     for line in fileobj:
+#         for token in line.split():
+#             yield token
+#
+#
+# def main():
+#     print("Hello, World!")
+#     filename = input("Please input the name of the file: ")
+#     print(filename, ' is the name of the file')
+#
+#     with open(filename, 'r') as file:
+#         words = read_word(file)
+#         for i in range(50):
+#             aword = next(words)
+#             print(aword)
+#
+#     print('goodbye')
+#
 
-        # firstword = next(words)
-        # secondword = next(words)
-        #
-        # print firstword, secondword
-        # for word in words:
-        #     print word
-
-
-
-
-    print 'goodbye'
 main()
-
